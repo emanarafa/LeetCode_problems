59ce71c17c07998de6cb6bebe99d7da85ba09162
eman arafa <eman.abourakha@gmail.com>
2023-06-02 15:36:29 +0200

    added new code

---
+++ Permutations_46.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations_46 {
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[]used=new boolean[nums.length];
+        ArrayList<Integer>permutation=new ArrayList<>();
+        List<List<Integer>>res=new ArrayList<>();
+       return backTrackPermut(nums,permutation,used,res);
+    }
+    public  List<List<Integer>> backTrackPermut(int[]nums, ArrayList<Integer> permutation, boolean[]used,List<List<Integer>>res){
+        if (permutation.size()==nums.length){
+           res.add(new ArrayList<>(permutation));
+             return res;
+        }
+        for (int i=0;i<nums.length;i++){
+            if (!used[i]){
+                permutation.add(nums[i]);
+                used[i]=true;
+                backTrackPermut(nums,permutation,used,res);
+                used[i]=false;
+                permutation.remove(permutation.size()-1);
+            }
+        }
+        return res;
+    }
+}
